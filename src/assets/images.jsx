fa19fb9e1ac22205b7f53e8f1abfc3263fc47bbf
Kii <56679633+KiixZ@users.noreply.github.com>
2024-09-11 21:11:21 +0700

    Create images.jsx

---
+++ src/assets/images.jsx
@@ -0,0 +1,17 @@
+import Daniel from './img/daniel.png';
+import Logo from './img/logonx.png';
+import Netflix from './img/netflix.png';
+import Shirley from './img/shirley.png';
+import Theo from './img/theo.png';
+import Why from './img/whyus.png';
+
+const images = {
+  Daniel
+  Logo
+  Netflix
+  Shirley
+  Theo
+  Why
+};
+
+export default images;
